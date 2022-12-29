1b6188914b2cd61e71c59b32cf6ee79df4293703
harismehar5 <harismehar3@gmail.com>
2022-12-29 12:11:53 +0500

    Bug Fixing

--- controllers/BlogController.js
+++ controllers/BlogController.js
@@ -91,7 +91,7 @@ exports.updateById = async (req, res) => {
 
 exports.updateStatus = async (req, res) => {
   try {
-    const response = await Job.updateOne(
+    const response = await Blog.updateOne(
       { _id: req.params.id },
       {
         $set: {
